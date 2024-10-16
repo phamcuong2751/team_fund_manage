e5812ebc91877f05656b7940045fdc8c996b5df5
Pham Phong Phu Cuong <phamcuong2751@gmail.com>
2024-10-16 09:01:08 +0700

    add Scheduler

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home';
-
+import MyScheduler from './pages/MyScheduler';
 
 const App = () => {
  
-
   return (
     <Router>
       <div>
         <Routes>
           <Route path="/" element={<Home  />} />
+          <Route path="/scheduler" element={<MyScheduler  />} />
         </Routes>
       </div>
     </Router>
